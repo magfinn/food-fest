94799d4acdd05ba50082b6c12ec9261e97a62ae1
Maggie Finnegan <wisner.maggiel@gmail.com>
2022-02-12 17:37:27 -0800

    cloned code for comparison

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
-const path = require('path');
 const webpack = require('webpack');
+const path = require('path');
 
-//entry= point at which to start (client's code), output = where we want it to go (dist folder), mode = what mode do we want it to run? (development or production). It runs at production automatically
 module.exports = {
   entry: './assets/js/script.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.join(__dirname + '/dist'),
     filename: 'main.bundle.js',
   },
   plugins: [
